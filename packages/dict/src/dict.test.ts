825107d4505c755064fcbae479331c7de37c24dd
Timo Sand <timo.sand@iki.fi>
2018-11-03 22:57:38 +0100

    Adds initial dict package

---
+++ packages/dict/src/dict.test.ts
@@ -0,0 +1,47 @@
+import { map } from './dict'
+
+describe('map', () => {
+  it('should transform items', () => {
+    const transformed = map({foo: 'bar'}, ([k,v]) => k+v)
+    expect(transformed).toEqual(['foobar'])
+  })
+
+  it('should list only values', () => {
+    const transformed = map({foo: 'bar'}, ([k,v]) => v)
+    expect(transformed).toEqual(['bar'])
+  })
+})
+
+// describe('filter', () => {
+//   it('should remove items not matching predicate', () => {
+//     const filtered = filter(['a', 'bb', 'cc', 'ddd'], s => s.length !== 2)
+//     expect(filtered).toEqual(['a', 'ddd'])
+//   })
+
+//   const isString = (s: number | string): s is string => typeof s === 'string'
+
+//   it('should return items matching guard as type of guard', () => {
+//     const filtered: string[] = filter([11, 'a', 'bb', 12, 'ddd', 33], isString)
+//     expect(filtered).toEqual(['a', 'bb', 'ddd'])
+//   })
+
+//   describe('filter.partial', () => {
+//     it('should return items matching guard as type of guard', () => {
+//       const items = [11, 'a', 'bb', 12, 'ddd', 33]
+//       const filtered: string[] = filter.partial(isString)(items)
+//       expect(filtered).toEqual(['a', 'bb', 'ddd'])
+//     })
+//   })
+// })
+
+// describe('reduce', () => {
+//   it('should sum values', () => {
+//     const result = reduce([2, 4, 6], 0, (r, n) => r + n)
+//     expect(result).toBe(12)
+//   })
+
+//   it('should work with empty array', () => {
+//     const result = reduce([], 0, (r, n) => r + n)
+//     expect(result).toBe(0)
+//   })
+// })
---
+++ packages/dict/src/dict.ts
@@ -0,0 +1,56 @@
+
+export const map = <A,B>(dict: A, mapfn: ([k, v] : [string, any]) => B): B[] => {
+  const objectEntries = Object.entries(dict)
+  const result = new Array<B>(objectEntries.length)
+  for (let i = 0; i < objectEntries.length; i++) {
+    result[i] = mapfn(objectEntries[i])
+  }
+  return result
+}
+
+// map.partial = <A, B>(mapFn: (a: A) => B) => (array: A[]): B[] =>
+//   map(array, mapFn)
+
+// export function filter<A, B extends A>(
+//   array: A[],
+//   guard: (value: A) => value is B
+// ): B[]
+// export function filter<A>(array: A[], predicate: (value: A) => boolean): A[]
+// export function filter(array: any[], predicate: (value: any) => boolean) {
+//   const result: any[] = []
+//   for (let i = 0; i < array.length; i++) {
+//     const value = array[i]
+//     if (predicate(value)) {
+//       result.push(value)
+//     }
+//   }
+//   return result
+// }
+
+// function filterPartial<A, B extends A>(
+//   predicate: (value: A) => value is B
+// ): (array: A[]) => B[]
+// function filterPartial<A>(predicate: (value: A) => boolean): (array: A[]) => A[]
+// function filterPartial(
+//   predicate: (value: any) => boolean
+// ): (array: any[]) => any[] {
+//   return (array: any[]) => filter(array, predicate)
+// }
+
+// filter.partial = filterPartial
+
+// export const reduce = <A, R>(
+//   array: A[],
+//   initial: R,
+//   reducer: (acc: R, value: A) => R
+// ) => {
+//   let result = initial
+//   for (let i = 0; i < array.length; i++) {
+//     result = reducer(result, array[i])
+//   }
+//   return result
+// }
+
+// reduce.partial = <A, R>(reducer: (acc: R, value: A) => R) => (initial: R) => (
+//   array: A[]
+// ) => reduce(array, initial, reducer)
