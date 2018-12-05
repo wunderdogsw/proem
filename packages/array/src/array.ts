a9edf6729ffc553c69a86b06eefadf4be1c53151
Timo Sand <timo.sand@iki.fi>
2018-12-05 10:13:37 +0100

    Removes unnecessary optional type from indices

--- packages/array/src/array.ts
+++ packages/array/src/array.ts
@@ -16,8 +16,8 @@ function mapPartial<A, B>( mapFn: (value: A, index?: number) => B ): (array: A[]
 map.partial = mapPartial
 
 export function filter<A, B extends A>(array: A[], guard: Guard<A, B>): B[]
-export function filter<A>(array: A[], predicate: (value: A, index?: number) => boolean): A[]
-export function filter(array: any[], predicate: (value: any, index?: number) => boolean) {
+export function filter<A>(array: A[], predicate: (value: A, index: number) => boolean): A[]
+export function filter(array: any[], predicate: (value: any, index: number) => boolean) {
          const result: any[] = []
          for (let i = 0; i < array.length; i++) {
            const value = array[i]
@@ -29,8 +29,8 @@ export function filter(array: any[], predicate: (value: any, index?: number) => 
        }
 
 function filterPartial<A, B extends A>(guard: Guard<A, B>): (array: A[]) => B[]
-function filterPartial<A>(predicate: (value: A, index?: number) => boolean): (array: A[]) => A[]
-function filterPartial(predicate: (value: any, index?: number) => boolean): (array: any[]) => any[] {
+function filterPartial<A>(predicate: (value: A, index: number) => boolean): (array: A[]) => A[]
+function filterPartial(predicate: (value: any, index: number) => boolean): (array: any[]) => any[] {
   return (array: any[]) => filter(array, predicate)
 }
 
@@ -39,7 +39,7 @@ filter.partial = filterPartial
 export const reduce = <A, R>(
   array: A[],
   initial: R,
-  reducer: (accumulator: R, value: A, index?: number) => R
+  reducer: (accumulator: R, value: A, index: number) => R
 ) => {
   let result = initial
   for (let i = 0; i < array.length; i++) {
@@ -48,6 +48,6 @@ export const reduce = <A, R>(
   return result
 }
 
-reduce.partial = <A, R>(reducer: (accumulator: R, value: A, index?: number) => R) => (initial: R) => (
+reduce.partial = <A, R>(reducer: (accumulator: R, value: A, index: number) => R) => (initial: R) => (
   array: A[]
 ) => reduce(array, initial, reducer)
