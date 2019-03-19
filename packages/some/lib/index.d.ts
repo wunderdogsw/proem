223844348c2c7058b0184ac3c9e8c65f28377db6
Joni Katajamäki <joni.katajamaki@gmail.com>
2019-03-19 22:38:56 +0200

    Ignore generated files from formatter

--- packages/some/lib/index.d.ts
+++ packages/some/lib/index.d.ts
@@ -1,5 +1,5 @@
 export declare const map: {
-  <A, B>(value: A | null, mapper: (value: A) => B): B | null
-  partial<A, B>(mapper: (value: A) => B): (value: A) => B | null
-}
+    <A, B>(value: A | null, mapper: (value: A) => B): B | null;
+    partial<A, B>(mapper: (value: A) => B): (value: A) => B | null;
+};
 //# sourceMappingURL=index.d.ts.map
