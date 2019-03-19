e5eec4700376c28d2d41250e60280f8e21f20996
Joni Katajamäki <joni.katajamaki@gmail.com>
2019-03-19 22:57:14 +0200

    Set node deps as externals with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,18 @@
+const path = require('path')
 const { references } = require('./tsconfig.json')
+const autoExternal = require('rollup-plugin-auto-external')
 
 const packagePaths = references.map(({ path }) => path)
 
-module.exports = packagePaths.map(path => ({
-  input: `${path}/lib/index.js`,
+module.exports = packagePaths.map(pkg => ({
+  input: `${pkg}/lib/index.js`,
   output: {
-    file: `${path}/lib/index.cjs.js`,
+    file: `${pkg}/lib/index.cjs.js`,
     format: 'cjs',
   },
+  plugins: [
+    autoExternal({
+      packagePath: path.resolve(__dirname, pkg, 'package.json'),
+    }),
+  ],
 }))
