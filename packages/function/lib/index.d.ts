e08cbf419dd01f3c38b2a9cd05d110b8d7694141
Joni Katajamäki <joni.katajamaki@wunderdog.fi>
2019-03-20 12:14:51 +0200

    Add function.partial

--- packages/function/lib/index.d.ts
+++ packages/function/lib/index.d.ts
@@ -3,4 +3,5 @@ export declare type Predicate<A> = (value: A) => boolean;
 export declare type UnaryFn<A, B> = (value: A) => B;
 export declare type BinaryFn<A, B, C> = (a: A, b: B) => C;
 export declare type Reducer<A, B> = (accumulator: B, value: A) => B;
+export declare function partial<A, B, Rest extends any[]>(fun: (a: A, ...rest: Rest) => B): (...rest: Rest) => (a: A) => B;
 //# sourceMappingURL=index.d.ts.map
--- packages/function/src/index.ts
+++ packages/function/src/index.ts
@@ -7,3 +7,16 @@ export type UnaryFn<A, B> = (value: A) => B
 export type BinaryFn<A, B, C> = (a: A, b: B) => C
 
 export type Reducer<A, B> = (accumulator: B, value: A) => B
+
+/**
+ * Transforms a function into a partially applied one.
+ * The transformed function takes the same arguments as the original function
+ * except for the first one, and returns a function that only takes the originals first argument.
+ */
+export function partial<A, B, Rest extends any[]>(
+  fun: (a: A, ...rest: Rest) => B,
+): (...rest: Rest) => (a: A) => B {
+  return (...rest: Rest) => (a: A): B => {
+    return fun(a, ...rest)
+  }
+}
