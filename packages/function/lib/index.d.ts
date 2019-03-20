aa3a49c739ee53e59ca57d642d2b276281196f26
Joni Katajamäki <joni.katajamaki@wunderdog.fi>
2019-03-20 12:18:26 +0200

    Don't remove comments from declaration files

--- packages/function/lib/index.d.ts
+++ packages/function/lib/index.d.ts
@@ -3,5 +3,10 @@ export declare type Predicate<A> = (value: A) => boolean;
 export declare type UnaryFn<A, B> = (value: A) => B;
 export declare type BinaryFn<A, B, C> = (a: A, b: B) => C;
 export declare type Reducer<A, B> = (accumulator: B, value: A) => B;
+/**
+ * Transforms a function into a partially applied one.
+ * The transformed function takes the same arguments as the original function
+ * except for the first one, and returns a function that only takes the originals first argument.
+ */
 export declare function partial<A, B, Rest extends any[]>(fun: (a: A, ...rest: Rest) => B): (...rest: Rest) => (a: A) => B;
 //# sourceMappingURL=index.d.ts.map
