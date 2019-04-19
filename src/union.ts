4056a558e0b51de1c74869d415dbad92b6a1fb21
Joni Katajamäki <joni.katajamaki@gmail.com>
2019-04-19 09:34:01 +0300

    docs(union): add jsdocs for exported items

--- src/union.ts
+++ src/union.ts
@@ -1,3 +1,4 @@
+/** `mapLiteral` transforms a literal type value by calling a case function that matches the value. */
 export function mapLiteral<Union extends string | number, Result>(
   label: Union,
   cases: { [Label in Union]: (label: Label) => Result },
