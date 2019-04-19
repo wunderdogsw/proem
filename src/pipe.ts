2d9b5ff296effec13e8bfd31882f9c45c3335e26
Joni Katajamäki <joni.katajamaki@gmail.com>
2019-04-19 09:31:30 +0300

    docs(pipe): add jsdocs for exported items

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,12 +1,18 @@
+/** `Pipe` is a function that can be composed with another function. */
 export interface Pipe<A, B> {
   (value: A): B
 
+  /**
+   * `to` takes a function and it's arguments after the first one. It returns
+   * a Pipe that transforms a value using the original pipe and the function as the last step.
+   */
   to<C, Args extends unknown[]>(
     fn: (value: B, ...args: Args) => C,
     ...args: Args
   ): Pipe<A, C>
 }
 
+/** `pipe` returns a composable pipeline function that takes a single argument. */
 export const pipe = <A, B, Rest extends unknown[]>(
   fn: (a: A, ...rest: Rest) => B,
   ...rest: Rest
