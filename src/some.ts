d8676ce8fa9e3898cc7302a7061ce5077b8cc5c8
Joni Katajamäki <joni.katajamaki@gmail.com>
2019-04-19 09:32:25 +0300

    docs(some): add jsdocs for exported items

--- src/some.ts
+++ src/some.ts
@@ -1,6 +1,3 @@
-/*
-  Package for handling possibly null values.
-*/
-
+/** `map` transforms a value with a function, or returns null if the value was null. */
 export const map = <A, B>(value: A | null, mapper: (value: A) => B): B | null =>
   value === null ? null : mapper(value)
